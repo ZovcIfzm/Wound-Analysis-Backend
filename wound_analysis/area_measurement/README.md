c021643cd807a5c31ce87053550ad12ffae11d17
Alex Ruan <alexruan2222@gmail.com>
2020-11-29 02:01:05 -0500

    removed deprecated data, changed constants

--- wound_analysis/area_measurement/README.md
+++
@@ -1,7 +0,0 @@
-# The project
-
-Automatically measuring area of wound
-
-# Most promising image so far
-
-![Alt text](promising1.jpg)
--- wound_analysis/manual/constants.py
+++ wound_analysis/manual/constants.py
@@ -1,29 +1,29 @@
 import numpy as np
 
-AREA_UPPER_LIMIT = 0.475
-AREA_LOWER_LIMIT = 0.025
+AREA_UPPER_LIMIT = 0.675
+AREA_LOWER_LIMIT = 0.005
 
-A_LR = [np.array([0, 45, 20]), np.array([140, 45, 20])]
-A_UR = [np.array([40, 255, 185]), np.array([180, 255, 185])]
+A_LR = [np.array([0, 75, 70]), np.array([160, 75, 70])]
+A_UR = [np.array([20, 255, 200]), np.array([180, 255, 200])]
 
-B_LR = [np.array([0, 75, 20]), np.array([150, 75, 20])]
-B_UR = [np.array([30, 255, 177]), np.array([180, 255, 177])]
+B_LR = [np.array([0, 95, 30]), np.array([150, 95, 30])]
+B_UR = [np.array([30, 255, 197]), np.array([180, 255, 197])]
 
 C_LR = [np.array([0, 100, 20]), np.array([150, 100, 20])]
 C_UR = [np.array([30, 255, 177]), np.array([180, 255, 177])]
 
 D_LR = [np.array([0, 105, 20]), np.array([150, 105, 20])]
-D_UR = [np.array([30, 255, 155]), np.array([180, 255, 155])]
+D_UR = [np.array([30, 255, 167]), np.array([180, 255, 167])]
 
 E_LR = [np.array([0, 110, 0]), np.array([150, 110, 0])]
-E_UR = [np.array([30, 255, 150]), np.array([180, 255, 150])]
+E_UR = [np.array([30, 255, 153]), np.array([180, 255, 153])]
 
 F_LR = [np.array([0, 110, 0]), np.array([150, 110, 0])]
-F_UR = [np.array([30, 255, 120]), np.array([180, 255, 120])]
+F_UR = [np.array([30, 255, 140]), np.array([180, 255, 140])]
 
 ORIG_LOWER_RANGE = [np.array([0, 30, 30]), np.array([130, 30, 30])]
 ORIG_UPPER_RANGE = [np.array([40, 255, 255]), np.array([180, 255, 255])]
 
 
-DEF_LOWER_RANGE = B_LR
-DEF_UPPER_RANGE = B_UR
+DEF_LOWER_RANGE = C_LR
+DEF_UPPER_RANGE = C_UR
