3410abb3f737024e82f8d2c0a86264eaf8bcfcdf
Alex Ruan <alexruan2222@gmail.com>
2021-01-20 02:54:12 -0500

    attempt minimal heroku build

--- wound_analysis/__init__.py
+++ wound_analysis/__init__.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 #app.config.from_object('wound_analysis.config')
 #app.config.from_envvar('PROJ_SETTINGS', silent=True)
-'''
+
 print('__init__.py ran manually')
 
 @wound_analysis.app.route('/testMain2/', methods=['POST', 'GET'])
 def testMain2():
     print('__init__.py ran')
     return "__init__.py ran in api"
-'''
-import wound_analysis.api
+
+#import wound_analysis.api
