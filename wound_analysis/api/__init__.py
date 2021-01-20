ae4d808bb860f5420007e410b1fe9e20e2942d13
Alex Ruan <alexruan2222@gmail.com>
2021-01-20 02:19:54 -0500

    potentially fixed heroku bug
    added routes

--- wound_analysis/api/__init__.py
+++ wound_analysis/api/__init__.py
@@ -1,1 +1,6 @@
+import wound_analysis.api.analysis
+import wound_analysis.api.constants
+import wound_analysis.api.helpers
+import wound_analysis.api.processing_helpers
+import wound_analysis.api.recog
 import wound_analysis.api.routes
