e4cf0378a2a6bf2216096fe3e3b108752e336010
Alex Ruan <alexruan2222@gmail.com>
2021-06-13 02:24:09 -0400

    gcloud deploy attempts

--- wound_analysis/__init__.py
+++ wound_analysis/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
-CORS(app, support_credentials=True)
+CORS(app)
 
 #app.config.from_object('wound_analysis.config')
 #app.config.from_envvar('PROJ_SETTINGS', silent=True)
--- wound_analysis/api/routes.py
+++ wound_analysis/api/routes.py
@@ -33,7 +33,7 @@ def get_current_time():
     return {'time': time.time()}
 
 @wound_analysis.app.route('/zipMeasure', methods=['POST', 'GET'])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def zipMeasure():
     
     fileobj = flask.request.files["file"]
@@ -93,7 +93,7 @@ def zipMeasure():
     return response
 
 @wound_analysis.app.route('/measure', methods=['POST', 'GET'])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def measure():
     # Retrieve fields
     width = float(flask.request.form.get("width"))
@@ -147,7 +147,7 @@ def measure():
 
 
 @wound_analysis.app.route('/testImage', methods=['POST', 'GET'])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def testImage():
 
     # Retrieve fields
@@ -166,7 +166,12 @@ def testImage():
     return response
 
 @wound_analysis.app.route('/', methods=['POST', 'GET'])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def hello():
     """Return a friendly HTTP greeting."""
     return "This is the wound analysis api"
+
+@wound_analysis.app.route("/testPage", methods=['POST', 'GET'])
+@cross_origin()
+def testPage():
+    return "<h1>Test Page</h1>"
