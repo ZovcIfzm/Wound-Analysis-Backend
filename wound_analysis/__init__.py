b1ecd66eeff8dd4c77289d2c40c139874e182bce
Alex Ruan <alexruan2222@gmail.com>
2021-06-24 22:08:01 -0400

    added CORS to backend

--- wound_analysis/__init__.py
+++ wound_analysis/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_object('wound_analysis.config')
 
 #app.config.from_object('wound_analysis.config')
